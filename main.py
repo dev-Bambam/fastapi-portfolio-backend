700b00b734c6808192a7d26e44ea14ffc560719f
Bambam <ayogood18@gmail.com>
2025-10-31 17:33:20 +0100

    added cors middleware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 
 from core.db import db_init
 from route import router as mainRoute
@@ -17,6 +18,21 @@ app = FastAPI()
 async def startup_event():
     # We call the async function using await
     await db_init()
+
+origins = [
+    "*",  # CRITICAL: Allows ALL origins (domains) to access the API. Use this for testing/development.
+    # In a production environment, you should replace "*" with a specific URL:
+    # "https://www.your-frontend-domain.com", 
+    # "http://localhost:8000", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,             # Allow cookies and credentials to be included in requests
+    allow_methods=["*"],                # Allow all standard HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],                # Allow all headers, especially required for 'Authorization' header
+)
 
     
 # Mount error handlers
